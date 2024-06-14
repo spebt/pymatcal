0cff8b157780fab09b4514ea4d850ae16b4470e5
Fang Han <fhan0904@gmail.com>
2024-06-14 15:55:15 -0400

    add coordinate transformation validation, test2.py

--- pymatcal/coord_transform.py
+++ pymatcal/coord_transform.py
@@ -1,14 +1,31 @@
 # 2d coordinate transformation, N x 3 Numpy array as input,
 # z coordinate is untouched
+# The coordinate frame rotates counter-clock-wise by angle_rad
 import math
-def coord_transform(angle_rad, x_shift, y_shift, input):
-    m_rotation = []
+import numpy as np
 
+
+def coord_transform_v2(angle_rad, x_shift, y_shift, input):
     # Rotational
     # Angle in radians
-    out[:, 0] = out[:, 0] * np.cos(angle_rad) + out[:, 1] * np.sin(angle_rad)
-    out[:, 1] = out[:, 1] * np.cos(angle_rad) - out[:, 0] * np.sin(angle_rad)
+    out = np.zeros((input.shape[0], 3))
+    # print(angle_rad)
+    out[:, 0] = input[:, 0] * np.cos(angle_rad) + input[:, 1] * np.sin(angle_rad)
+    out[:, 1] = input[:, 1] * np.cos(angle_rad) - input[:, 0] * np.sin(angle_rad)
+    # print(out)
     # Translational transformation
     out[:, 0] = out[:, 0] - x_shift
     out[:, 1] = out[:, 1] + y_shift
     return out
+
+
+def get_mtransform(angle_rad, x_shift, y_shift):
+    return (np.array([[math.cos(angle_rad), -math.sin(angle_rad), 0],
+                     [math.sin(angle_rad), math.cos(angle_rad), 0],
+                      [0, 0, 1]]), np.array([-x_shift, y_shift, 0]))
+
+# M(x+M_inv*a)=Mx+a
+
+
+def coord_transform(m_rotate, m_shift, input):
+    return np.matmul(input, m_rotate) + m_shift
--- test2.py
+++ test2.py
@@ -1,10 +1,46 @@
 import pymatcal
 import numpy as np
-angle_rad =0
-x_shift=10
-y_shift=10
-trans_x = 10
-trans_y =10
-input_np=np.array([[5,5,0]])
-print(pymatcal.coord_transform(angle_rad, x_shift, y_shift, trans_x, trans_y, input_np))
-print("Done!")
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+angle_rad = np.pi/6.0
+x_shift = 0
+y_shift = 0
+input_np = np.array([[5, 5, 0]])
+# print(pymatcal.coord_transform_v2(angle_rad, x_shift, y_shift, input_np))
+mr, mt = pymatcal.get_mtransform(angle_rad, x_shift, y_shift)
+# print(pymatcal.coord_transform(mr,mt,input_np))
+
+
+# randomly generated points
+rng = np.random.default_rng()
+npx = 4
+ydata = rng.integers(low=0, high=180, size=npx)
+xdata = rng.integers(low=0, high=180, size=npx)
+data1 = np.vstack([xdata, ydata, np.zeros(npx)]).T
+# rads1 = np.arctan(data1[:,0]-90)/(data1[:,1]-90)
+data2 = pymatcal.coord_transform(
+    mr, mt, data1-np.array((90, 90, 0)))+np.array((90, 90, 0))
+mr, mt = pymatcal.get_mtransform(0, 20, -30)
+data3 = pymatcal.coord_transform(
+    mr, mt, data1-np.array((90, 90, 0)))+np.array((90, 90, 0))
+fig, axs = plt.subplots(1,2,figsize=(16, 10))
+for ax in axs:
+    ax.set_xlim(-70, 250)
+    ax.set_ylim(-70, 250)
+    ax.set_aspect('equal')
+axs[0].add_patch(mpl.patches.Polygon(data1[:, 0:2], fc='orange'))
+axs[0].add_patch(mpl.patches.Polygon(data2[:, 0:2], alpha=0.5, fc='blue'))
+axs[0].plot([data1[0, 0], 90],
+        [data1[0, 1], 90], c='k',ls='--')
+axs[0].plot([data2[0, 0], 90],
+        [data2[0, 1], 90], c='k',ls='--')
+axs[0].add_patch(mpl.patches.Circle((90, 90), 2, fc='k'))
+
+axs[1].add_patch(mpl.patches.Polygon(data1[:, 0:2], fc='orange'))
+axs[1].add_patch(mpl.patches.Polygon(data3[:, 0:2], alpha=0.5, fc='blue'))
+axs[1].plot([data1[0, 0], data3[0,0]],
+        [data1[0, 1], data3[0, 1]], c='k',ls='--')
+# axs[1].plot([data2[0, 0], 90],
+#         [data3[0, 1], 90], c='k',ls='--')
+axs[1].add_patch(mpl.patches.Circle((90, 90), 2, fc='k'))
+plt.show()
